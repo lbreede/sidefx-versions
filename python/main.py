5a8750fa87aaaad0a4945167f0a938dbf9c7a7ff
lbreede <lennart.breede@gmail.com>
2022-11-28 21:00:27 -0800

    2022-11-28

--- python/main.py
+++ python/main.py
@@ -1,12 +1,9 @@
 import re
-from pprint import pprint
-from typing import Optional
-import json
-import yaml
-import toml
 import os
+import json
 
-OS = "macosx"
+# Typing
+from typing import TextIO
 
 HEADINGS = {f"Houdini {x}" for x in {"19.5", "19.0", "18.5", "18.0", "17.5", "17.0"}}
 
@@ -20,50 +17,87 @@ SUBHEADINGS = {
     "Production Builds - Python 3",
 }
 
+# RegEx
+REGEX_HOUDINI_FILE = (
+    r"houdini(-|-py\d+-)((\d+)\.(\d+)\.(\d+))-([a-z0-9]+).+\.(dmg|tar\.gz|exe)"
+)
+REGEX_DOCKER_FILE = r"Houdini((\d+)\.(\d+)\.(\d+))_Docker\.zip"
+REGEX_SIDEFXLABS_FILE = r"SideFXLabs((\d+)\.(\d+)\.(\d+))\.zip"
 
-def parse_html(file):
-    re_hou = r"houdini(-|-py\d+-)(\d+)\.(\d+)\.(\d+)-([a-z0-9]+).+\.(dmg|tar\.gz|exe)"
-    re_dck = r"Houdini(\d+)\.(\d+)\.(\d+)_Docker\.zip"
-    re_lbs = r"SideFXLabs(\d+)\.(\d+)\.(\d+)\.zip"
 
-    data = {}
+def parse_html(html: TextIO) -> dict:
+    data: dict = {}
 
     current_heading = None
     current_subheading = None
 
-    for i, row in enumerate(file):
+    for i, row in enumerate(html):
 
         row = row.strip()
 
         if row in HEADINGS:
             current_heading = row.replace(" ", "_")
             data[current_heading] = {}
+            continue
 
         if row in SUBHEADINGS and current_heading in data:
             current_subheading = row.replace(" ", "_").replace(".", "_")
             data[current_heading][current_subheading] = []
+            continue
 
-        if re.search(re_hou, row) or re.search(re_dck, row) or re.search(re_lbs, row):
-            data[current_heading][current_subheading].append(row)
+        if re.search(REGEX_HOUDINI_FILE, row):
+            data[current_heading][current_subheading].append(houdini(row))
+        elif re.search(REGEX_DOCKER_FILE, row):
+            data[current_heading][current_subheading].append(docker(row))
+        elif re.search(REGEX_SIDEFXLABS_FILE, row):
+            data[current_heading][current_subheading].append(sidefxlabs(row))
 
     return data
 
 
-def dump(data, filename, method="json"):
+def houdini(file: str, expand_version=False) -> dict:
+    match = re.search(REGEX_HOUDINI_FILE, file)
+    python, version, major, minor, patch, platform, _ = match.groups()
 
-    os.mkdir("../json") if not os.path.isdir("../json") else None
-    os.mkdir("../yaml") if not os.path.isdir("../yaml") else None
-    os.mkdir("../toml") if not os.path.isdir("../toml") else None
+    python = (
+        None
+        if python == "-"
+        else float(re.search(r"-py(\d+)-", python).group(1).ljust(2, "0")) * 0.1
+    )
+    if expand_version:
+        version = _expand_version(version, major, minor, patch)
 
-    if method.lower() in ("json", "j"):
-        with open(f"../json/{filename}.json", "w") as fp:
-            json.dump(data, fp, indent=4)
-    elif method.lower() in ("yaml", "y"):
-        with open(f"../yaml/{filename}.yaml", "w") as fp:
-            yaml.dump(data, fp)
-    elif method.lower() in ("toml", "t"):
-        with open(f"../toml/{filename}.toml", "w") as fp:
-            toml.dump(data, fp)
+    return {"version": version, "python": python, "platform": platform, "file": file}
+
+
+def docker(file: str, expand_version=False) -> dict:
+    match = re.search(REGEX_DOCKER_FILE, file)
+    ver, major, minor, patch = match.groups()
+    ver = _expand_version(ver, major, minor, patch) if expand_version else ver
+    return {"version": ver, "file": file}
+
+
+def sidefxlabs(file: str, expand_version=False) -> dict:
+    match = re.search(REGEX_SIDEFXLABS_FILE, file)
+    ver, major, minor, patch = match.groups()
+    ver = _expand_version(ver, major, minor, patch) if expand_version else ver
+    return {"version": ver, "file": file}
+
+
+def _expand_version(version, major, minor, patch):
+    major, minor, patch = (int(x) for x in (major, minor, patch))
+    return {"version": version, "major": major, "minor": minor, "patch": patch}
+
+
+def dump(data: dict, filename: str) -> None:
+
+    directory = os.path.join("..", "json")
+    os.mkdir(directory) if not os.path.isdir(directory) else None
+    file = filename + "." + "json"
+    path = os.path.join(directory, file)
+
+    with open(path, "w") as fp:
+        json.dump(data, fp, indent=4)
 
 
 def main():
@@ -71,34 +105,7 @@ def main():
     with open("../html/Daily Builds SideFX.html") as fp:
         data = parse_html(fp)
 
-    dump(data, "test", method="j")
-    dump(data, "test", method="y")
-    dump(data, "test", method="t")
-
-    # for line in html:
-
-    # houdini_all = re.findall(r'<a href=".+">\n.+(houdini-.+\..+)\n.+</a>', html)
-
-    # regex = (
-    #     r"houdini(-|-py\d+-)(\d+\.\d+\.\d+)-(macosx|linux|win64).+\.(dmg|tar\.gz|exe)"
-    # )
-
-    # for ver in houdini_all:
-    #     match = re.search(regex, ver)
-    #     file = match.group()
-    #     python, version, os, _ = match.groups()
-    #     python = (
-    #         None
-    #         if python == "-"
-    #         else float(python.removeprefix("-py").removesuffix("-").ljust(4, "0"))
-    #         * 0.001
-    #     )
-    #     major, minor, patch = [int(x) for x in version.split(".")]
-    #     # python = fmt_pyver(python)
-    #     # print(python)
-
-    #     if os == "macosx" and major == 19 and python == 3.7:
-    #         print(f"houdini-{version}")
+    dump(data, "builds")
 
 
 if __name__ == "__main__":
